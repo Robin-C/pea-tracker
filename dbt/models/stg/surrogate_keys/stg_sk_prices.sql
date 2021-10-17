117b96f6a11a02bfcbe49c30c5656058290492c0
Robin CHESNE <rr.chesne@gmail.com>
2021-10-17 18:25:20 +0000

    renamed column

--- dbt/models/stg/surrogate_keys/stg_sk_prices.sql
+++ dbt/models/stg/surrogate_keys/stg_sk_prices.sql
@@ -1,5 +1,5 @@
 with source_data as (
-select stg_prices.pk, stg_prices.date, stg_prices.adj_close, stg_prices.loaded_at,tickers_snapshot.dbt_scd_id as ticker_surrkey
+select stg_prices.pk, stg_prices.date, stg_prices.adj_close, stg_prices.loaded_at,tickers_snapshot.dbt_scd_id as ticker_sk
 from {{ ref('stg_prices') }} stg_prices
 left join {{ ref('tickers_snapshot') }} tickers_snapshot on stg_prices.ticker_id =tickers_snapshot.ticker_id 
 where stg_prices.Date between tickers_snapshot.dbt_valid_from and coalesce(tickers_snapshot.dbt_valid_to, '2999-12-31')
