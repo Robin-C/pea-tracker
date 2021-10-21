28817a763f9053f82b92fc2383fec452b485b11a
Robin C <r.chesne@gmail.com>
2021-10-21 14:25:37 +0200

    rewrote monthly aggregations

--- dbt/models/stg/prices/aggregated/stg_prices_monthly_performance.sql
+++ dbt/models/stg/prices/aggregated/stg_prices_monthly_performance.sql
@@ -5,27 +5,32 @@ with monthly_performance as (
     group by 1,2
 ),
 
-price_on_last_day_of_prev_month as (
+/* We get the price of the last day of the month so we can calculate the price at the beginning of the month.
+price at the beginning of the period = price of last day of month - performance during the month.
+last day of month = first day next month minus 1
+DATE_SUB(DATE_TRUNC(DATE_ADD(CURRENT_DATE(), INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY)
+if last day of the month is at future date (ie we are still halfway thru the month) then take ticker value at current_date())
+*/
+price_on_last_day_current__month as (
     select year_month, prices.ticker_sk
-    , prev_month_prices.adj_close as price_last_month
+    , price_on_last_day_current__month.adj_close as price_last_day_of_month
     from {{ ref('stg_prices_daily_performance') }} prices
-    inner join {{ ref('stg_prices_daily_performance') }} prev_month_prices on prev_month_prices.date = date_trunc(prices.date, month) - 1 and prices.ticker_sk = prev_month_prices.ticker_sk
+    inner join {{ ref('stg_prices_daily_performance') }} price_on_last_day_current__month on price_on_last_day_current__month.date = case when DATE_SUB(DATE_TRUNC(DATE_ADD(prices.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) > current_date() then current_date() else DATE_SUB(DATE_TRUNC(DATE_ADD(prices.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) end and prices.ticker_sk = price_on_last_day_current__month.ticker_sk
     inner join {{ ref('stg_dates') }} on prices.date = date_id
     where 1=1
-    and prices.date = date_trunc(prices.date, month)
-
+    and prices.date = case when DATE_SUB(DATE_TRUNC(DATE_ADD(prices.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) > current_date() then current_date() else DATE_SUB(DATE_TRUNC(DATE_ADD(prices.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) end
 ),
 
 
 final as (
-    select price_on_last_day_of_prev_month.*
+    select price_on_last_day_current__month.*
            , monthly_performance.monthly_performance
-           , (price_on_last_day_of_prev_month.price_last_month + monthly_performance.monthly_performance) / price_on_last_day_of_prev_month.price_last_month - 1 as monthly_performance_percent
-           , concat(price_on_last_day_of_prev_month.year_month, price_on_last_day_of_prev_month.ticker_sk) as pk
-    from price_on_last_day_of_prev_month
-    inner join monthly_performance on monthly_performance.year_month = price_on_last_day_of_prev_month.year_month 
-    and monthly_performance.ticker_sk = price_on_last_day_of_prev_month.ticker_sk
-    order by price_on_last_day_of_prev_month.year_month
+           , monthly_performance.monthly_performance / (price_on_last_day_current__month.price_last_day_of_month - monthly_performance.monthly_performance)  as monthly_performance_percent
+           , concat(price_on_last_day_current__month.year_month, price_on_last_day_current__month.ticker_sk) as pk
+    from price_on_last_day_current__month
+    inner join monthly_performance on monthly_performance.year_month = price_on_last_day_current__month.year_month 
+    and monthly_performance.ticker_sk = price_on_last_day_current__month.ticker_sk
+    order by price_on_last_day_current__month.year_month
 )
 
 select *
--- dbt/models/stg/transactions/aggregated/stg_portfolio_monthly_performance.sql
+++ dbt/models/stg/transactions/aggregated/stg_portfolio_monthly_performance.sql
@@ -6,14 +6,19 @@ with monthly_portfolio_performance as (
     group by 1,2
 ),
 
-/* then we need to get the portfolio value on the last day of the previous month so we can calculate % increase/decrease */
-/* portfolio value on the last day of the month is cum_qty */
-portfolio_value_on_last_day_of_prev_month as (
-    select year_month, porfolio_daily_performance.ticker_sk, prev_porfolio_daily_performance.ticker_value
+/* We get the ticker value of the last day of the month so we can calculate the ticker value at the beginning of the month.
+ticker value at the beginning of the month = ticker value of last day of month - performance during the month.
+last day of month = first day next month minus 1
+DATE_SUB(DATE_TRUNC(DATE_ADD(CURRENT_DATE(), INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY)
+if last day of the month is at future date (ie we are still halfway thru the month) then take ticker value at current_date())
+*/
+
+portfolio_value_on_last_day_of_current_month as (
+    select year_month, porfolio_daily_performance.ticker_sk, portfolio_value_on_last_day_of_current_month.ticker_value as ticker_value_last_day_of_month
     from {{ ref('stg_portfolio_daily_performance') }} porfolio_daily_performance
-    inner join {{ ref('stg_portfolio_daily_performance') }} prev_porfolio_daily_performance on prev_porfolio_daily_performance.date = date_trunc(porfolio_daily_performance.date, month) - 1 and porfolio_daily_performance.ticker_sk = prev_porfolio_daily_performance.ticker_sk
-    inner join {{ref('stg_dates') }} on porfolio_daily_performance.date = date_id
-    where porfolio_daily_performance.date = date_trunc(porfolio_daily_performance.date, month) 
+    inner join {{ ref('stg_portfolio_daily_performance') }} portfolio_value_on_last_day_of_current_month on portfolio_value_on_last_day_of_current_month.date = case when DATE_SUB(DATE_TRUNC(DATE_ADD(porfolio_daily_performance.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) > current_date() then current_date() else DATE_SUB(DATE_TRUNC(DATE_ADD(porfolio_daily_performance.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) end and porfolio_daily_performance.ticker_sk = portfolio_value_on_last_day_of_current_month.ticker_sk
+    inner join {{ ref('stg_dates') }} on porfolio_daily_performance.date = date_id
+    where porfolio_daily_performance.date = case when DATE_SUB(DATE_TRUNC(DATE_ADD(porfolio_daily_performance.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) > current_date() then current_date() else DATE_SUB(DATE_TRUNC(DATE_ADD(porfolio_daily_performance.date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) end
     order by year_month
 ),
 
@@ -21,11 +26,12 @@ portfolio_value_on_last_day_of_prev_month as (
 /* We join the 2 tables together and finally calculate the % increase */
 final as (
     select monthly_portfolio_performance.year_month, monthly_portfolio_performance.ticker_sk
+    , ticker_value_last_day_of_month
     , monthly_portfolio_performance.monthly_performance
-    , monthly_performance / ticker_value as monthly_portfolio_performance_percent
+    , monthly_performance / (ticker_value_last_day_of_month - monthly_performance) as monthly_portfolio_performance_percent
     , concat(monthly_portfolio_performance.year_month, monthly_portfolio_performance.ticker_sk) as pk
     from monthly_portfolio_performance 
-    inner join portfolio_value_on_last_day_of_prev_month on monthly_portfolio_performance.ticker_sk = portfolio_value_on_last_day_of_prev_month.ticker_sk and monthly_portfolio_performance.year_month = portfolio_value_on_last_day_of_prev_month.year_month
+    inner join portfolio_value_on_last_day_of_current_month on monthly_portfolio_performance.ticker_sk = portfolio_value_on_last_day_of_current_month.ticker_sk and monthly_portfolio_performance.year_month = portfolio_value_on_last_day_of_current_month.year_month
 )
 
 select *
