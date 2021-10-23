fce36fea17952f7028635c8fd2c28628511be8b2
Robin C <r.chesne@gmail.com>
2021-10-23 17:54:57 +0200

    added some tickers

--- extract_load_scripts/get_tickers/get_tickers.py
+++ extract_load_scripts/get_tickers/get_tickers.py
@@ -5,34 +5,28 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/root/.dbt/pea-tracker-5b072ff47
 import sys
 from datetime import datetime
 
-#path needs to be absolute so it gets executed correctly by the worker. For some reason..
+# path needs to be absolute so it gets executed correctly by the worker. For some reason..
 
-#open xlsx file
-read_file = pd.read_excel ('/my_app/extract_load_scripts/get_tickers/input/ticker_list.xlsx')
+# read file
+df=pd.read_csv('/my_app/extract_load_scripts/get_tickers/input/ticker_list.csv')
 
-#save file to csv
-read_file.to_csv ('/my_app/extract_load_scripts/get_tickers/output/ticker_list.csv', index = None, header=True)
-
-#read file
-df=pd.read_csv('/my_app/extract_load_scripts/get_tickers/output/ticker_list.csv')
-
-#add loaded_at col
+# add loaded_at col
 df['loaded_at'] = datetime.now()
 
-#instantiate connection to bigquery
+# instantiate connection to bigquery
 client = bigquery.Client()
 
-#specify bigquery dataset
+# specify bigquery dataset
 dataset_ref = client.dataset('sources')
 
-#specify bigquery table
+# specify bigquery table
 table_ref = dataset_ref.table('tickers')
 
-#delete table
+# delete table
 query_str = """delete from sources.tickers where true"""
 ticker_array = (
     client.query(query_str)
 )
 
-#send it to bigquery
+# send it to bigquery
 client.load_table_from_dataframe(df, table_ref).result()
