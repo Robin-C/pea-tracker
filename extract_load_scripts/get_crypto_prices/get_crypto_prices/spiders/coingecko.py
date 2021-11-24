65f31d54037c2d6bfad23d4711f97defb896a1ff
Robin C <r.chesne@gmail.com>
2021-11-24 13:08:16 +0100

    cleaned script

--- extract_load_scripts/get_crypto_prices/get_crypto_prices/spiders/coingecko.py
+++ extract_load_scripts/get_crypto_prices/get_crypto_prices/spiders/coingecko.py
@@ -6,45 +6,41 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/root/.dbt/pea-tracker-5b072ff47
 import pandas as pd
 
 class pmuSpider(scrapy.Spider):
-    name = "coingecko"
-    rows = list()
-    bqclient = bigquery.Client()
+    name = 'coingecko'
 
-    def start_requests(self):
-        # Get the coin list from bigquery
-        query_str = """select coinGecko_name
+    def __init__(self):
+      self.rows = list()  # That list will be used to store prices before being injected in a dataframe
+      self.bqclient = bigquery.Client()
+      # Get the coin list from bigquery
+      self.query_str = """select coinGecko_name
         from sources.coins"""
-        response = (
-            self.bqclient.query(query_str)
+      self.response = (
+            self.bqclient.query(self.query_str)
             .result()
         )
+      self.urls = list()  # Create list to compile url list to be scraped
+      self.base_url = 'https://www.coingecko.com/en/coins/'
 
-        urls = list()  # Create list to compile url list to be scraped
-        base_url = 'https://www.coingecko.com/en/coins/'
- 
-        for coin in response:
-          urls.append(
-            { 'url': base_url + coin.get('coinGecko_name'),
+    def start_requests(self):
+        for coin in self.response:
+          self.urls.append(
+            { 'url': self.base_url + coin.get('coinGecko_name'),
             'coin': coin.get('coinGecko_name')}
           )
-
-        for url in urls:
+        for url in self.urls:
           yield scrapy.Request(url=url['url'], callback=self.parse, meta={'coin': url['coin']})
 
     def parse(self, response):
-      price = response.xpath('//html/body/div[4]/div[4]/div[1]/div/div[1]/div[4]/div/div[1]/span[1]/span/text()').get()
+      price = float(response.xpath('//html/body/div[4]/div[4]/div[1]/div/div[1]/div[4]/div/div[1]/span[1]/span/text()').get().replace('$', '').replace(',',''))
       coin = response.meta['coin']
       date_of_the_day = datetime.today().strftime('%Y-%m-%d')
       loaded_at = datetime.now()
-      self.rows.append([date_of_the_day, response.meta['coin'], price, loaded_at])
-      print(self.rows)
+      self.rows.append([date_of_the_day, coin, price, loaded_at])
 
     def closed(self, reason):
       if reason == 'finished':
-        dataset_ref = self.bqclient.dataset('sources')  # specify bigquery dataset
-        table_ref = dataset_ref.table('prices_crypto') # specify bigquery table
-        # send it to bigquery
-        df = pd.DataFrame(self.rows, columns =['date', 'coin', 'price', 'loaded_at'], dtype = float)
-        df['date'] = pd.to_datetime(df["date"]).dt.date
-        self.bqclient.load_table_from_dataframe(df, table_ref).result()
-        print(df)
+        dataset_ref = self.bqclient.dataset('sources')
+        table_ref = dataset_ref.table('prices_crypto')
+        df = pd.DataFrame(self.rows, columns =['date', 'coin', 'price', 'loaded_at'])
+        df['date'] = pd.to_datetime(df["date"]).dt.date  # Transform col to date type
+        self.bqclient.load_table_from_dataframe(df, table_ref).result()  # Send it to BQ
